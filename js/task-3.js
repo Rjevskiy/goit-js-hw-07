f8f2b14db9352791bac46c777a1afa85855bb963
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-10-06 14:41:16 +0300

    ы

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+const inputEl = document.getElementById('name-input');
+const outputEl = document.getElementById('name-output');
+
+inputEl.addEventListener('input', () => {
+  const name = inputEl.value;
+  
+
+  if (name === '' || /^\s*$/.test(name)) {
+    outputEl.textContent = 'Anonymous';
+  } else {
+    outputEl.textContent = name;
+  }
+});
+
+
+
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input) підставляє його поточне значення в span#name - output як ім’я для привітання.
+// Обов’язково очищай значення в інпуті по краях від пробілів.
+// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
